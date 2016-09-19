d97f51babb4d2023033142122500fb68a37fa409
Hossain Khan <amardeshbd@gmail.com>
2016-09-19 19:54:23 -0400

    [ADDED] Initial structured files generated from android studio.

---
+++ core-lib/src/main/java/info/hossainkhan/android/core/CoreConfig.java
@@ -0,0 +1,8 @@
+package info.hossainkhan.android.core;
+
+import com.example.ApiCore;
+
+public class CoreConfig {
+    public static final String NAME = "TESTING";
+    public static final String LIB_DEPS = ApiCore.TEST;
+}
