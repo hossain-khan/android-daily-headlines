78d327fc88483345c6373cfb73332190da1f9fa9
Hossain Khan <amardeshbd@gmail.com>
2016-10-11 19:50:12 -0400

    [ADDED] [#55] Added  from leanback library for settings menu items.

---
+++ tv/src/main/java/info/hossainkhan/dailynewsheadlines/ImageCardViewPresenter.java
@@ -0,0 +1,74 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Hossain Khan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package info.hossainkhan.dailynewsheadlines;
+
+import android.content.Context;
+import android.support.v17.leanback.widget.ImageCardView;
+import android.view.ContextThemeWrapper;
+
+import com.squareup.picasso.Picasso;
+
+import io.swagger.client.model.Article;
+
+/**
+ * A very basic {@link ImageCardView} {@link android.support.v17.leanback.widget.Presenter}.You can
+ * pass a custom style for the ImageCardView in the constructor. Use the default constructor to
+ * create a Presenter with a default ImageCardView style.
+ */
+public class ImageCardViewPresenter extends AbstractCardPresenter<ImageCardView> {
+
+    public ImageCardViewPresenter(Context context, int cardThemeResId) {
+        super(new ContextThemeWrapper(context, cardThemeResId));
+    }
+
+    public ImageCardViewPresenter(Context context) {
+        this(context, R.style.DefaultCardTheme);
+    }
+
+    @Override
+    protected ImageCardView onCreateView() {
+        ImageCardView imageCardView = new ImageCardView(getContext());
+//        imageCardView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Toast.makeText(getContext(), "Clicked on ImageCardView", Toast.LENGTH_SHORT).show();
+//            }
+//        });
+        return imageCardView;
+    }
+
+    @Override
+    public void onBindViewHolder(final Article article, final ImageCardView cardView) {
+        cardView.setTag(article);
+        cardView.setTitleText(article.getTitle());
+        cardView.setContentText(article.getAbstract());
+        if (article.getMultimedia() != null && article.getMultimedia().size() > 4) {
+            Picasso.with(getContext())
+                    .load(article.getMultimedia().get(3).getUrl())
+                    .into(cardView.getMainImageView());
+        }
+    }
+
+}
