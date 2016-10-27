64529790d30f99a5d0824099e69e79d5b9b26510
Hossain Khan <amardeshbd@gmail.com>
2016-10-27 18:09:56 -0400

    [CLEANUP] [#80] Removed toast message now that we dont have the action to `READ MORE`.

--- tv/src/main/java/info/hossainkhan/dailynewsheadlines/details/HeadlinesDetailsFragment.java
+++ tv/src/main/java/info/hossainkhan/dailynewsheadlines/details/HeadlinesDetailsFragment.java
@@ -181,7 +181,9 @@ public class HeadlinesDetailsFragment extends DetailsFragment implements Headlin
             String logMsg = "App does not have browser to show URL: %s.";
             Timber.w(logMsg, contentUrl);
             FirebaseCrash.log(logMsg);
-            UiUtils.showToast(mApplicationContext, R.string.warning_no_browser_app_available);
+
+            // NOTE: According to Google's guideline and test case "TV-WB", tv app
+            // should not assume browser availability.
         }
     }
 }
