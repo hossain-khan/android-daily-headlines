ea7b9fa73bdaff503ff1e45d053638c530e6b099
Hossain Khan <amardeshbd@gmail.com>
2016-11-07 17:10:00 -0500

    [ADDED] Preference based news source storage provider.

---
+++ core-lib/src/main/java/info/hossainkhan/android/core/usersource/UserSourceManager.java
@@ -0,0 +1,102 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2016 Hossain Khan
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+
+package info.hossainkhan.android.core.usersource;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.reflect.TypeToken;
+
+import java.lang.reflect.Type;
+import java.util.HashMap;
+import java.util.Map;
+
+import info.hossainkhan.android.core.util.Validate;
+import timber.log.Timber;
+
+/**
+ * Persistant user's {@link UserSourceProvider} using simple shared preferences.
+ */
+public class UserSourceManager implements UserSourceProvider {
+
+    private static final String PREF_KEY_NEWS_SOURCES = "PREF_KEY_user_news_source_feeds_map";
+    private Map<String, String> mNewsMap;
+    private final SharedPreferences mSharedPreferences;
+
+    /**
+     * Creates user's news source manager.
+     *
+     * @param context Application context.
+     */
+    public UserSourceManager(final Context context) {
+        mSharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
+        loadSources();
+    }
+
+    @Override
+    public void addSource(final String title, final String url) {
+        Validate.notNull(title, url);
+        mNewsMap.put(url, title);
+        updateSources();
+    }
+
+    @Override
+    public String removeSource(final String url) {
+        String removedSourceTitle = mNewsMap.remove(url);
+        updateSources();
+        return removedSourceTitle;
+    }
+
+    @Override
+    public Map<String, String> getSources() {
+        return mNewsMap;
+    }
+
+
+    //
+    // Internal methods
+    //
+
+    private void updateSources() {
+        Gson gson = new GsonBuilder().create();
+        String serializedJson = gson.toJson(mNewsMap);
+
+        SharedPreferences.Editor preferencesEditor = mSharedPreferences.edit();
+        preferencesEditor.putString(PREF_KEY_NEWS_SOURCES, serializedJson);
+        preferencesEditor.apply();
+    }
+
+    private void loadSources() {
+        String string = mSharedPreferences.getString(PREF_KEY_NEWS_SOURCES, "{}");
+        Timber.d("Saving news sources: ");
+
+        Gson gson = new GsonBuilder().create();
+        Type hasMapTypeToken = new TypeToken<Map<String, String>>() { }.getType();
+        mNewsMap = gson.fromJson(string, hasMapTypeToken); // This type must match TypeToken
+    }
+}
