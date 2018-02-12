fcd0fa40cbfe472f2bf5cc239d5ea66b1e18d35e
Hossain Khan <amardeshbd@gmail.com>
2018-02-11 20:45:36 -0500

    [ADDED] Files from navigation drawer widzard. Needs cleanup.

--- mobile/src/main/java/info/hossainkhan/dailynewsheadlines/HeadlinesBrowseActivity.kt
+++ mobile/src/main/java/info/hossainkhan/dailynewsheadlines/HeadlinesBrowseActivity.kt
@@ -26,12 +26,24 @@ package info.hossainkhan.dailynewsheadlines
 
 
 import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+
+import android.support.design.widget.NavigationView
+import android.support.v4.view.GravityCompat
+import android.support.v7.app.ActionBarDrawerToggle
+import android.support.v7.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.view.View
+import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_headlines_browse.*
+import kotlinx.android.synthetic.main.activity_headlines_browse_viewpager.*
+import kotlinx.android.synthetic.main.app_bar_main.*
 
-class HeadlinesBrowseActivity : Activity() {
+
+class HeadlinesBrowseActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
+
 
     /**
      * The [android.support.v4.view.PagerAdapter] that will provide
@@ -46,6 +58,16 @@ class HeadlinesBrowseActivity : Activity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_headlines_browse)
+        setSupportActionBar(toolbar)
+
+
+        val toggle = ActionBarDrawerToggle(
+                this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+        drawer_layout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        nav_view.setNavigationItemSelectedListener(this)
+
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         headlinesPagerAdapter = HeadlinesPagerAdapter(fragmentManager, MainActivity.selectedCards)
@@ -54,6 +76,13 @@ class HeadlinesBrowseActivity : Activity() {
         container.adapter = headlinesPagerAdapter
     }
 
+    override fun onBackPressed() {
+        if (drawer_layout.isDrawerOpen(GravityCompat.START)) {
+            drawer_layout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -65,14 +94,28 @@ class HeadlinesBrowseActivity : Activity() {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        val id = item.itemId
-
-        if (id == R.id.action_settings) {
-            return true
+        when (item.itemId) {
+            R.id.action_settings -> return true
+            else -> return super.onOptionsItemSelected(item)
         }
-
-        return super.onOptionsItemSelected(item)
     }
 
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        // Handle navigation view item clicks here.
+        when (item.itemId) {
+            R.id.nav_camera -> {
+                // Handle the camera action
+            }
+            R.id.nav_share -> {
 
+            }
+            R.id.nav_send -> {
+
+            }
+        }
+
+        drawer_layout.closeDrawer(GravityCompat.START)
+        return true
+    }
 }
+
